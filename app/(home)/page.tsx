b5898c3aac6ab3eb14f94cde6cd782d2f4531f83
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-05-05 23:27:15 +0100

    07-create-todays-deals-slider

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,10 +2,13 @@ import { HomeCarousel } from "@/components/shared/home/home-carousel"
 import data from "@/lib/data"
 import {
   getAllCategories,
+  getProductsByTag,
   getProductsForCard,
 } from "@/lib/actions/product.actions"
 import { toSlug } from "@/lib/utils"
 import { HomeCard } from "@/components/shared/home/home-card"
+import { Card, CardContent } from "@/components/ui/card"
+import ProductSlider from "@/components/shared/product/product-slider"
 
 export default async function Home() {
   const categories = (await getAllCategories()).slice(0, 4)
@@ -51,11 +54,18 @@ export default async function Home() {
       },
     },
   ]
+
+  const todaysDeals = await getProductsByTag({ tag: "todays-deal" })
   return (
     <>
       <HomeCarousel items={data.carousels} />
       <div className='md:p-4 md:space-y-4 bg-border'>
         <HomeCard cards={cards} />
+        <Card className='w-full rounded-none'>
+          <CardContent className='p-4 items-center gap-3'>
+            <ProductSlider title="Today's Deals" products={todaysDeals} />
+          </CardContent>
+        </Card>
       </div>
     </>
   )

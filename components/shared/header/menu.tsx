f586fcf71baf7b5a15ffd184a7be4e6d2fcbd314
Hermann2710 <138356589+Hermann2710@users.noreply.github.com>
2025-05-05 10:55:57 +0100

    04-create-home-page-carousel

--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -5,7 +5,7 @@ export default function Menu() {
   return (
     <div className='flex justify-end'>
       <nav className='flex gap-3 w-full'>
-        <Link href='/cart' className='flex items-center header-button'>
+        <Link href='/signin' className='flex items-center header-button'>
           Hello, Sign in
         </Link>
 
